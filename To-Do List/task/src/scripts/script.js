3fad490fff80df97577172b1c1c7b06aa63278d7
Peter Stackebrandt <peter.stackebrandt@gmail.com>
2021-10-14 16:28:05 +0200

    todo list stage 2 start

---
+++ To-Do List/task/src/scripts/script.js
@@ -0,0 +1,12 @@
+function addTask() {
+    // get data from #input-task
+    // create new task
+    // clear content of input-task
+}
+
+// create task, add to list
+function createTask() {
+    // create task
+    // add task to list
+}
+
